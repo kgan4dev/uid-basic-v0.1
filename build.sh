5df4f8dda59892a410f60ee4a88ecaf060c247a6
GANESH KAILA <kailaganesh07@gmail.com>
2015-07-07 15:00:54 +0530

    Initial commit

--- build.sh
+++ build.sh
@@ -14,9 +14,9 @@ fi
 printf "Building started for AllJoyn component services.\n"
 cd $path/core/alljoyn/core/alljoyn/
 if [ $TARGET_CPU == 'x86' ]; then
-	scons WS=off BINDINGS=cpp SERVICES="about,notification,onboarding,controlpanel"
+	scons WS=off BINDINGS=cpp SERVICES="about,notification,controlpanel"
 elif [ $TARGET_CPU == 'arm' ]; then
-	scons WS=off BINDINGS=cpp CPU=arm OE_BASE=/usr SERVICES="about,notification,onboarding,controlpanel"
+	scons WS=off BINDINGS=cpp CPU=arm OE_BASE=/usr SERVICES="about,notification,controlpanel"
 fi
 
 printf "Checking Alljoyn About Service . . .			"
@@ -43,18 +43,22 @@ else
 	exit
 fi
 
-printf "Checking Alljoyn Onboarding Client Service . . .	"
-if [ -f $path/core/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/onboarding/bin/OnboardingClient ]; then
-	if [ -f $path/core/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/onboarding/bin/onboarding-daemon ]; then 
-		printf "Build successful.!\n"
-	else
-		printf "\nSomething went wrong.!\n"
-		exit
-	fi
-else
-	printf "\nSomething went wrong.!\n"
-	exit
-fi
+#printf "Checking Alljoyn Onboarding Client Service . . .	"
+#if [ -f $path/core/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/onboarding/bin/OnboardingClient ]; then
+#	printf "Build successful.!\n"
+#else
+#	printf "\nSomething went wrong.!\n"
+#	exit
+#fi
+
+#printf "Checking Alljoyn Onboarding Daemon Service . . .        "
+#if [ -f $path/core/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/onboarding/bin/onboardingdaemon ]; then
+#       printf "Build successful.!\n"
+#else
+#       printf "\nSomething went wrong.!\n"
+#       exit
+#fi
+
 
 printf "Checking Alljoyn Control Panel Service . . .		"
 if [ -f $path/core/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/controlpanel/bin/ControlPanelController ]; then
--- run.sh
+++ run.sh
@@ -17,7 +17,7 @@ fi
 
 # Running Alljoyn component services
 if [ $arg == 'start' ]; then
-	export LD_LIBRARY_PATH=$path/core/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/cpp/lib:$path/core/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/about/lib:$path/core/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/notification/lib:$path/core/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/onboarding/lib:$path/core/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/config/lib:$$LD_LIBRARY_PATH
+	export LD_LIBRARY_PATH=$path/core/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/cpp/lib:$path/core/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/about/lib:$path/core/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/notification/lib:$path/core/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/services_common/lib:$path/core/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/onboarding/lib:$path/core/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/config/lib:$path/core/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/controlpanel/lib:$LD_LIBRARY_PATH
 
 	printf "Starting Alljoyn component services . . . \n"
 
@@ -39,12 +39,30 @@ if [ $arg == 'start' ]; then
                 printf "Done\n"
 	fi
 
-	printf "\nOnboarding Client service	\n"
-	if [ ! -z "$(ps | grep "OnboardingClientSample" | awk '{print $1}')" ];then
+#	printf "\nOnboarding Client service	\n"
+#	if [ ! -z "$(ps | grep "OnboardingClien" | awk '{print $1}')" ];then
+#		printf "Service already running. . . \n"
+#	else
+#		printf "Service Starting. . .                   "
+#		$path/core/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/onboarding/bin/OnboardingClient >> $path/logs/alljoyn/OnboardingClient.log 2>&1 &
+#                printf "Done\n"
+#	fi
+
+#	printf "\nOnboarding Daemon service	\n"
+#	if [ ! -z "$(ps | grep "onboardingdaemo" | awk '{print $1}')" ];then
+#		printf "Service already running. . . \n"
+#	else
+#		printf "Service Starting. . .                   "
+#		$path/core/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/onboarding/bin/onboardingdaemon >> $path/logs/alljoyn/Onboardingdaemon.log 2>&1 &
+#                printf "Done\n"
+#	fi
+
+	printf "\nControl Panel service	\n"
+	if [ ! -z "$(ps | grep "ControlPanelCon" | awk '{print $1}')" ];then
 		printf "Service already running. . . \n"
 	else
 		printf "Service Starting. . .                   "
-		$path/core/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/onboarding/bin/OnboardingClient >> $path/logs/alljoyn/OnboardingClient.log 2>&1 &
+		$path/core/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/controlpanel/bin/ControlPanelController >> $path/logs/alljoyn/ControlPanelController.log 2>&1 &
                 printf "Done\n"
 	fi
 
@@ -53,7 +71,7 @@ if [ $arg == 'start' ]; then
 #	printf "Done\n\n"
 	printf "\nAlljoyn component services started Successfully.!\n"
 elif [ $arg == 'restart' ];then
-	export LD_LIBRARY_PATH=$path/components/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/cpp/lib:$path/components/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/about/lib:$path/components/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/notification/lib:$path/core/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/onboarding/lib:$path/core/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/config/lib:$LD_LIBRARY_PATH
+	export LD_LIBRARY_PATH=$path/core/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/cpp/lib:$path/core/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/about/lib:$path/core/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/notification/lib:$path/core/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/services_common/lib:$path/core/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/onboarding/lib:$path/core/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/config/lib:$path/core/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/controlpanel/lib:$LD_LIBRARY_PATH
 
 	printf "Restarting Alljoyn component services . . . \n"
 
@@ -85,17 +103,45 @@ elif [ $arg == 'restart' ];then
                 printf "Done\n"
 	fi
 
-	printf "\nOnboarding Client service	\n"
-	if [ ! -z "$(ps | grep "OnboardingClient" | awk '{print $1}')" ];then
+#	printf "\nOnboarding Client service	\n"
+#	if [ ! -z "$(ps | grep "OnboardingClien" | awk '{print $1}')" ];then
+#		printf "Service Stopping. . .			"
+#		kill -9 $(ps | grep "OnboardingClien" | awk '{print $1}')
+#		printf "Done\n"
+#		printf "Service Starting. . . 			"
+#		$path/core/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/onboarding/bin/OnboardingClient >> $path/logs/alljoyn/OnboardingClient.log 2>&1 &
+#		printf "Done\n"
+#	else
+#		printf "Service Starting. . .                   "
+#		$path/core/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/onboarding/bin/OnboardingClient >> $path/logs/alljoyn/OnboardingClient.log 2>&1 &
+#                printf "Done\n"
+#	fi
+
+#	printf "\nOnboarding Daemon service	\n"
+#	if [ ! -z "$(ps | grep "onboardingdaemo" | awk '{print $1}')" ];then
+#		printf "Service Stopping. . .			"
+#		kill -9 $(ps | grep "onboardingdaemo" | awk '{print $1}')
+#		printf "Done\n"
+#		printf "Service Starting. . . 			"
+#		$path/core/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/onboarding/bin/onboardingdaemon >> $path/logs/alljoyn/onboardingdaemon.log 2>&1 &
+#		printf "Done\n"
+#	else
+#		printf "Service Starting. . .                   "
+#		$path/core/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/onboarding/bin/onboardingdaemon >> $path/logs/alljoyn/onboardingdaemon.log 2>&1 &
+#                printf "Done\n"
+#	fi
+
+	printf "\nControl Panel service	\n"
+	if [ ! -z "$(ps | grep "ControlPanelCon" | awk '{print $1}')" ];then
 		printf "Service Stopping. . .			"
-		kill -9 $(ps | grep "OnboardingClient" | awk '{print $1}')
+		kill -9 $(ps | grep "ControlPanelCon" | awk '{print $1}')
 		printf "Done\n"
 		printf "Service Starting. . . 			"
-		$path/core/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/onboarding/bin/OnboardingClient >> $path/logs/alljoyn/OnboardingClient.log 2>&1 &
+		$path/core/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/controlpanel/bin/ControlPanelController >> $path/logs/alljoyn/ControlPanelController.log 2>&1 &
 		printf "Done\n"
 	else
 		printf "Service Starting. . .                   "
-		$path/core/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/onboarding/bin/OnboardingClient >> $path/logs/alljoyn/OnboardingClient.log 2>&1 &
+		$path/core/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/controlpanel/bin/ControlPanelController >> $path/logs/alljoyn/ControlPanelController.log 2>&1 &
                 printf "Done\n"
 	fi
 
@@ -116,15 +162,6 @@ elif [ $arg == 'stop' ]; then
 		printf "Done\n"
 	fi
 
-	printf "\nOnboarding Client service\n"
-	if [ -z $(ps | grep "OnboardingClient" | awk '{print $1}') ];then
-		printf "Service found not running . . .\n"
-	else
-		printf "Service stopping. . .			"
-		kill -9 $(ps | grep "OnboardingClient" | awk '{print $1}')
-		printf "Done\n"
-	fi
-
 	printf "\nNotification Consumer Service\n"
 	if [ -z $(ps | grep "ConsumerService" | awk '{print $1}') ];then
 		printf "Service found not running . . .\n"
@@ -133,6 +170,34 @@ elif [ $arg == 'stop' ]; then
 		kill -9 $(ps | grep "ConsumerService" | awk '{print $1}')
 		printf "Done\n"
 	fi
+
+#	printf "\nOnboarding Client service\n"
+#	if [ -z $(ps | grep "OnboardingClien" | awk '{print $1}') ];then
+#		printf "Service found not running . . .\n"
+#	else
+#		printf "Service stopping. . .			"
+#		kill -9 $(ps | grep "OnboardingClien" | awk '{print $1}')
+#		printf "Done\n"
+#	fi
+
+#	printf "\nOnboarding Daemon service\n"
+#	if [ -z $(ps | grep "onboardingdaemo" | awk '{print $1}') ];then
+#		printf "Service found not running . . .\n"
+#	else
+#		printf "Service stopping. . .			"
+#		kill -9 $(ps | grep "onboardingdaemo" | awk '{print $1}')
+#		printf "Done\n"
+#	fi
+
+	printf "\nControl Panel Service\n"
+	if [ -z $(ps | grep "ControlPanelCon" | awk '{print $1}') ];then
+		printf "Service found not running . . .\n"
+	else
+		printf "Service stopping. . .			"
+		kill -9 $(ps | grep "ControlPanelCon" | awk '{print $1}')
+		printf "Done\n"
+	fi
+
 #	printf "Notification Producer Service. . . 	"
 #	kill -9 $(ps | grep "ProducerService" | awk '{print $1}')
 #	printf "Done\n\n"
@@ -145,15 +210,29 @@ elif [ $arg == "status" ];then
                 printf "Service is running . . .\n"
         fi
 
-        printf "\nOnboarding Client service\n"
-        if [ -z $(ps | grep "OnboardingClient" | awk '{print $1}') ];then
+        printf "\nNotification Consumer Service\n"
+        if [ -z $(ps | grep "ConsumerService" | awk '{print $1}') ];then
                 printf "Service found not running . . .\n"
         else
                 printf "Service is running . . .\n"
         fi
 
-        printf "\nNotification Consumer Service\n"
-        if [ -z $(ps | grep "ConsumerService" | awk '{print $1}') ];then
+#        printf "\nOnboarding Client service\n"
+#        if [ -z $(ps | grep "OnboardingClien" | awk '{print $1}') ];then
+#                printf "Service found not running . . .\n"
+#        else
+#                printf "Service is running . . .\n"
+#        fi
+
+#        printf "\nOnboarding Daemon service\n"
+#        if [ -z $(ps | grep "onboardingdaemo" | awk '{print $1}') ];then
+#                printf "Service found not running . . .\n"
+#        else
+#                printf "Service is running . . .\n"
+#        fi
+
+        printf "\nControl Panel Service\n"
+        if [ -z $(ps | grep "ControlPanelCon" | awk '{print $1}') ];then
                 printf "Service found not running . . .\n"
         else
                 printf "Service is running . . .\n"
