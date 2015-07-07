7c987e73484e7f22b9c35e45ed65c00de858caee
GANESH KAILA <kailaganesh07@gmail.com>
2015-07-07 11:22:24 +0530

    Initial commit

--- build.sh
+++ build.sh
@@ -14,21 +14,13 @@ fi
 printf "Building started for AllJoyn component services.\n"
 cd $path/core/alljoyn/core/alljoyn/
 if [ $TARGET_CPU == 'x86' ]; then
-	scons WS=off BINDINGS=cpp SERVICES="about,notification,onboarding"
+	scons WS=off BINDINGS=cpp SERVICES="about,notification,onboarding,controlpanel"
 elif [ $TARGET_CPU == 'arm' ]; then
-	scons WS=off BINDINGS=cpp CPU=arm OE_BASE=/usr SERVICES="about,notification,onboarding"
+	scons WS=off BINDINGS=cpp CPU=arm OE_BASE=/usr SERVICES="about,notification,onboarding,controlpanel"
 fi
 
-printf "\nChecking Alljoyn About Service . . .			"
+printf "Checking Alljoyn About Service . . .			"
 if [ -f $path/core/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/about/bin/AboutClient ]; then
-	printf "Build successful.!\n"
-else
-	printf "\nSomething went wrong.!\n"
-	exit
-fi
-
-printf "\nChecking Alljoyn Onboarding Client Service . . .	"
-if [ -f $path/core/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/onboarding/bin/OnboardingClient ]; then
 	printf "Build successful.!\n"
 else
 	printf "\nSomething went wrong.!\n"
@@ -45,11 +37,32 @@ fi
 
 printf "Checking Alljoyn notification Producer Service . . .	"
 if [ -f $path/core/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/notification/bin/ProducerService ]; then
-	printf "Build successful.!\n\n"
+	printf "Build successful.!\n"
+else
+	printf "\nSomething went wrong.!\n"
+	exit
+fi
+
+printf "Checking Alljoyn Onboarding Client Service . . .	"
+if [ -f $path/core/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/onboarding/bin/OnboardingClient ]; then
+	if [ -f $path/core/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/onboarding/bin/onboarding-daemon ]; then 
+		printf "Build successful.!\n"
+	else
+		printf "\nSomething went wrong.!\n"
+		exit
+	fi
+else
+	printf "\nSomething went wrong.!\n"
+	exit
+fi
+
+printf "Checking Alljoyn Control Panel Service . . .		"
+if [ -f $path/core/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/controlpanel/bin/ControlPanelController ]; then
+	printf "Build successful.!\n"
 else
 	printf "\nSomething went wrong.!\n"
 	exit
 fi
 
 cd $path
-printf "Alljoyn component services build successfully Done.!\n"
+printf "\nAlljoyn component services build successfully Done.!\n"
