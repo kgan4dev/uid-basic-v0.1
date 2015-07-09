f5ef235be536f8d6adff5acb323a4cf08eb9a183
Ganesh <kgan4dev@gmail.com>
2015-07-09 05:31:08 +0000

    Initial commit

--- build.sh
+++ build.sh
@@ -4,19 +4,23 @@
 path=`pwd`
 arch=$(uname -m)
 
-if [ $arch == 'i686' ]; then
-	TARGET_CPU='x86'
-elif [ $arch == 'armv61' ]; then
-	TARGET_CPU='arm'
+if [ "$arch" == "i686" ]; then
+	TARGET_CPU="x86"
+elif [ "$arch" == "armv6l" ]; then
+	TARGET_CPU="arm"
+else
+	printf "\nSomething went wrong at '$arch' condition check\n"
 fi
 
 # building alljoyn component services
 printf "Building started for AllJoyn component services.\n"
 cd $path/core/alljoyn/core/alljoyn/
-if [ $TARGET_CPU == 'x86' ]; then
+if [ "$TARGET_CPU" == "x86" ]; then
 	scons WS=off BINDINGS=cpp SERVICES="about,notification,controlpanel"
-elif [ $TARGET_CPU == 'arm' ]; then
+elif [ "$TARGET_CPU" == "arm" ]; then
 	scons WS=off BINDINGS=cpp CPU=arm OE_BASE=/usr SERVICES="about,notification,controlpanel"
+else
+	printf "\nSomething went wrong at '$TARGET_CPU' condition check\n"
 fi
 
 printf "Checking Alljoyn About Service . . .			"
